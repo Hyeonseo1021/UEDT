1ede39f6ce7f5db27833b6518a146a3837ee8e09
Hyeonseo1021 <khs26319@naver.com>
2026-05-25 09:48:53 +0900

    Initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Vite + React</h1>
+        <p>Initial setup ready.</p>
+      </div>
+    </>
+  )
+}
+
+export default App
